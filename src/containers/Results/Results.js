dd11fec632ed3c99e5ca3bb501d2d66c782144df
Benjamin <Redhaus@Benjamins-MacBook-Pro.local>
2018-03-24 23:52:56 -0700

    fixed results layout

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -1,47 +1,125 @@
 import React from "react";
 
-import { Card, Col, Row, Divider } from "antd";
+import { Card, Col, Row, Divider, Icon } from "antd";
+
+let loadHeight = 0;
+
+var divHeight = {};
 
 class Results extends React.Component {
+
+
+
+
+
+  constructor(props){
+    super(props);
+
+    this.handlePrint = this.handlePrint.bind(this)
+  }
+
+
   handleSelect = (e, data) => {
     if (e.stopPropagation) e.stopPropagation();
     this.props.savePerformance(data);
   };
 
+
+state = {
+  heightStyle: {
+    height: 300
+  }
+}
+  
+  // // width and height in pixels, including padding and border
+  // // Corresponds to jQuery outerWidth(), outerHeight()
+  // var width = box.offsetWidth;
+  // var height = box.offsetHeight;
+  
+  // // width and height in pixels, including padding, but without border
+  // // Corresponds to jQuery innerWidth(), innerHeight()
+  // var width = box.clientWidth;
+  // var height = box.clientHeight;
+
+  componentDidMount(){
+    
+    let dynamicHeight = 0;
+    
+  const box = document.querySelectorAll('.rContainer').forEach( (item, key) => {
+    dynamicHeight += item.offsetHeight;
+  });
+
+  loadHeight = dynamicHeight / 2 + 200;
+
+  this.setState( () => {
+    return {
+      heightStyle: { height: loadHeight}
+    }
+
+  })
+  // divHeight = {
+  //   height: '300px'
+  // }
+  
+  // console.log(box)
+
+  // box.map
+
+
+
+  console.log(dynamicHeight);
+  
+
+
+  }
+
+
+  handlePrint = () => {
+    window.print();
+
+   
+      // myDivToPrint.focus();
+      // myDivToPrint.print();
+     
+
+
+  }
+
+
   renderPrimary = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
-          <h5>{item.title}</h5>
+        <div className="" key={key}>
+          <span className="results-title">{item.title}</span >
           <p>
             {item.author} | {item.date}
           </p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderPrimary()
 
   renderLexis = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
+        <div className=" results-lexis" key={key}>
           <p>
             {item.word} ({item.pos})
           </p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderLexis()
 
   renderReadings = (poems, essays, movies) => {
     if (poems) {
       return poems.map((item, key) => {
         console.log(item);
         return (
-          <div key={key}>
-            <h3>{item.title}</h3>
+          <div className="" key={key}>
+            <span className="results-title">{item.title}</span>
             <p>{item.author}</p>
           </div>
         );
@@ -52,8 +130,8 @@ class Results extends React.Component {
       return essays.map((item, key) => {
         console.log(item);
         return (
-          <div key={key}>
-            <h3>{item.title}</h3>
+          <div className="" key={key}>
+            <span className="results-title">{item.title}</span>
             <p>{item.author}</p>
           </div>
         );
@@ -64,93 +142,95 @@ class Results extends React.Component {
       return movies.map((item, key) => {
         console.log(item);
         return (
-          <div key={key}>
-            <h3>{item.title}</h3>
+          <div className="" key={key}>
+            <span className="results-title">{item.title}</span>
             <p>{item.director}</p>
           </div>
         );
       }); // end map()
     }
-  }; // end handle()
+  }; // end renderReadings()
 
   renderQuestions = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
+        <div className=" results-questions" key={key}>
           <p>{item}</p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderQuestions()
 
   renderPerformance = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
-          <p>{item.header}</p>
+        <div className="" key={key}>
+          <span className="results-title">{item.header}</span>
           <p>{item.description}</p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderPerformance()
 
   renderExtensions = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
+        <div className=" results-extensions" key={key}>
           <p>{item.ext}</p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderExtensions()
 
   renderGoals = results => {
     return results.map((item, key) => {
       console.log(item);
       return (
-        <div key={key}>
+        <div className=" results-goals" key={key}>
           <p>{item}</p>
         </div>
       );
     }); // end map()
-  }; // end handle()
+  }; // end renderGoals()
 
   renderOutline = (outlines) => {
 
-console.log('name: ' , outlines.tName)
+    console.log('name: ' , outlines.tName)
 
     return (
-    <div>
-    <p>{outlines.cName}</p>
+    <div className=" results-outline">
+    <label>Teacher:</label>
     <p>{outlines.tName}</p>
+
+    <label>Class:</label>
+    <p>{outlines.cName}</p>
+
+    <label>Week Summary:</label>
     <p>{outlines.summary}</p>
+
+    <label>Additional Notes:</label>
     <p>{outlines.notes}</p>
     
+   
+    <label>Specific Goals:</label>
     {outlines.goals 
     ? 
     outlines.goals.map( (goal, key) => {
       return (
-            <div key={key}>
-            <p>{goal.goal}</p>
-            <p>{goal.date.format('l')}</p>
+            
+            <div className="results-outline-goals" key={key}>
+            <p>{goal.goal} | {goal.date.format('l')}</p>
             </div>
           )
       })
-    
     : ''}
+    </div>
    
-    
-     
-    
-    
-   
-        </div>
-   
-  )
-  }; // end handle()
+    )
+    }; // end renderOutline()
 
 
 
@@ -171,38 +251,73 @@ console.log('name: ' , outlines.tName)
     return (
       // Card Grid
 
-      <div>
-        <Card className="results-container">
+      <div className="myDivToPrint">
+        <Card className="results-header" >
           <h3>Weekly Review</h3>
+
+          <Icon onClick={this.handlePrint} type="printer" className="hard-right" />
         </Card>
-        <div className="results-container">
-          <Card>
-            <Row gutter={16}>
-              <Col span={12}>
+        <div className="results-container" style={this.state.heightStyle}>
+          {/* <Card> */}
+            {/* <Row gutter={16}>
+              <Col span={12}> */}
+
+
+
+
+
+                 {/* Outline goals */}
+              
+                 {weekOutlineSelect.tName ? 
+                  <div className="rContainer results-card">
+                  <div>
+                  {console.log('log week: ' , weekOutlineSelect)}
+                    <Divider orientation="right">Weekly Outline</Divider>
+                    {this.renderOutline(weekOutlineSelect)}
+                  </div>
+                  </div>
+
+                 : '' }
+
+
+
                 {/* Primary Reading */}
+
+
+              
                 {primarySelect.length > 0 ? (
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Primary Readings</Divider>
                     {this.renderPrimary(primarySelect)}
                   </div>
-                ) : (
-                  ""
-                )}
-
-                {/* Lexis */}
-                {lexisSelect.length > 0 ? (
-                  <div>
-                    <Divider orientation="right">Lexis</Divider>
-                    {this.renderLexis(lexisSelect)}
                   </div>
                 ) : (
                   ""
                 )}
+               
+
+                {/* Lexis */}
+               
+                {lexisSelect.length > 0 ? (
+                  <div className="rContainer results-card">
+                  <div>
+                    <Divider orientation="right">Lexis</Divider>
+                    {this.renderLexis(lexisSelect)}
+                  </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+               
 
                 {/* Further Readings */}
+                
                 {poemSelect.length > 0 ||
                 essaySelect.length > 0 ||
                 movieSelect.length > 0 ? (
+
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Further Readings</Divider>
 
@@ -236,61 +351,76 @@ console.log('name: ' , outlines.tName)
                       " "
                     )}
                   </div>
+                  </div>
                 ) : (
                   ""
                 )}
-              </Col>
+               
+              {/* </Col> */}
 
-              <Col span={12}>
+              {/* <Col span={12}> */}
                 {/* Key Questions */}
+                
                 {questionSelect.length > 0 ? (
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Key Questions</Divider>
                     {this.renderQuestions(questionSelect)}
                   </div>
+                  </div>
                 ) : (
                   ""
                 )}
+               
+
 
                 {/* Performance */}
+                
                 {performanceSelect.length > 0 ? (
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Performances</Divider>
                     {this.renderPerformance(performanceSelect)}
                   </div>
+                  </div>
                 ) : (
                   ""
                 )}
+                
+
+
 
                 {/* Extensions */}
+                
                 {extensionSelect.length > 0 ? (
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Extensions</Divider>
                     {this.renderExtensions(extensionSelect)}
                   </div>
+                  </div>
                 ) : (
                   ""
                 )}
+               
+
 
                 {/* Continual goals */}
+                
                 {goalSelect.length > 0 ? (
+                  <div className="rContainer results-card">
                   <div>
                     <Divider orientation="right">Continual Goals</Divider>
                     {this.renderGoals(goalSelect)}
                   </div>
+                  </div>
                 ) : (
                   ""
                 )}
+               
 
-                {weekOutlineSelect.tName ? 
+
                 
-                  <div>
-                  {console.log('log week: ' , weekOutlineSelect)}
-                    <Divider orientation="right">Weekly Outline</Divider>
-                    {this.renderOutline(weekOutlineSelect)}
-                  </div>
-
-                 : '' }
 
                  {/* {console.log(weekOutlineSelect.tName)} */}
                
@@ -303,9 +433,9 @@ console.log('name: ' , outlines.tName)
                 )} */}
 
 
-              </Col>
-            </Row>
-          </Card>
+              {/* </Col>
+            </Row> */}
+          {/* </Card> */}
         </div>
       </div>
     );
